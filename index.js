a72d7d7282ce44fb7cba7a99d960284b59ff4dff
xcx-7 <zainab.42847@gmail.com>
2025-03-13 15:18:30 +0000

    updated

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ const cheerio = require("cheerio")
 const PORT = 8000
 const app = express()
 
+const url = 'https://techcrunch.com/'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        console.log(html)
+        const $  = cheerio.load(html)
+        const articles = []
+         $('.wp-block-heading', html).each(function() { 
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({ 
+                title,
+                url
+            })
+          })
+     })
+
 app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
